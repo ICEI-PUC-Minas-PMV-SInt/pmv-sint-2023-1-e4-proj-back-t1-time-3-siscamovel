2193719bbbc95cd828c1eaf5ff1b87f591434c14
Danilo Santos <artededan@protonmail.ch>
2023-04-08 00:18:30 -0300

    (SISCM-DM) Models: Adicione classe para especificações do veiculo

---
+++ Models/Especificacoes/Veiculos.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace siscm_data_management.Models.Especificacoes;
+
+public class Veiculos
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Fabricante")]
+    public string Fabricante { get; set; } = string.Empty;
+
+    [BsonElement("Modelo")]
+    public string Modelo { get; set; } = string.Empty;
+
+    [BsonElement("Ano")]
+    public int Ano { get; set; }
+}
