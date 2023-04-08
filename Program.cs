10bd51eca291c962e8063574cb5861d8100093a7
Danilo Santos <artededan@protonmail.ch>
2023-04-08 00:16:49 -0300

    (SISCM-DM) Database: Configure conexão com o MongoDB

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using siscm_data_management.Database;
+using siscm_data_management.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<DatabaseSettings>
+    (builder.Configuration.GetSection("SISCMDatabase"));
+builder.Services.AddSingleton<VeiculoServices>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
