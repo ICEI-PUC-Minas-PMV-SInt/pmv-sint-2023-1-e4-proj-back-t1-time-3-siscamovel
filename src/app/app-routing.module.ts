a6ade2255cd072d37f63710851e99410602a1a5d
Danilo Santos <artededan@protonmail.ch>
2023-04-09 21:06:56 -0300

    (motomami) app routing: admin: Adicione paths para AdminHome e ListarVeiculos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './components/admin/admin-home/admin-home.component';
+import { ListarVeiculosComponent } from './components/admin/listar-veiculos/listar-veiculos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/home',
+    component: AdminHomeComponent
+  },  
+  {
+    path: 'admin/listarveiculos',
+    component: ListarVeiculosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
