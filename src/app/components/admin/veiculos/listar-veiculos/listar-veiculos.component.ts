791d81c4852717f70e0a8629100cabe5b2d93cd1
Danilo Santos <artededan@protonmail.ch>
2023-04-20 08:51:58 -0300

    (motomami) components: listar-veiculos: mova a lista para o caminho veículos (1 de 3)

--- src/app/components/admin/veiculos/listar-veiculos/listar-veiculos.component.ts
+++ src/app/components/admin/veiculos/listar-veiculos/listar-veiculos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ListarVeiculos } from 'src/app/models/admin/listar-veiculos/listar-veiculos.model';
-import { ListarVeiculosService } from 'src/app/services/admin/listar-veiculos.service';
+import { ListarVeiculos } from 'src/app/models/admin/veiculos/listar-veiculos/listar-veiculos.model';
+import { ListarVeiculosService } from 'src/app/services/admin/veiculos/listar-veiculos/listar-veiculos.service';
 
 @Component({
   selector: 'app-listar-veiculos',
