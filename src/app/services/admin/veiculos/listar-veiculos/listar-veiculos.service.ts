0e0763af005e2f7d9895732c5ce5bdcf3236aacb
Danilo Santos <artededan@protonmail.ch>
2023-04-20 08:54:28 -0300

    (motomami) services: listar-veiculos: mova a lista para o caminho veículos (3 de 3)

--- src/app/services/admin/veiculos/listar-veiculos/listar-veiculos.service.ts
+++ src/app/services/admin/veiculos/listar-veiculos/listar-veiculos.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ListarVeiculos } from 'src/app/models/admin/listar-veiculos/listar-veiculos.model';
+import { ListarVeiculos } from 'src/app/models/admin/veiculos/listar-veiculos/listar-veiculos.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
