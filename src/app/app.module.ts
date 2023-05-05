6b7b600a313e2312a5a75b6403eb96c353999144
Danilo Santos <artededan@protonmail.ch>
2023-05-05 20:28:55 -0300

    (anything but me) module: declare componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './components/admin/login/login.component';
+import { HomeComponent } from './components/admin/home/home.component';
+import { AdminComponent } from './components/admin/gerenciar/admin/admin.component';
+import { PessoasComponent } from './components/admin/gerenciar/pessoas/pessoas.component';
+import { VeiculosComponent } from './components/admin/gerenciar/veiculos/veiculos.component';
+import { PeopleComponent } from './components/worldwide/people/people.component';
+import { FooterComponent } from './components/general/footer/footer.component';
+import { HeaderComponent } from './components/general/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminComponent,
+    PessoasComponent,
+    VeiculosComponent,
+    PeopleComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
