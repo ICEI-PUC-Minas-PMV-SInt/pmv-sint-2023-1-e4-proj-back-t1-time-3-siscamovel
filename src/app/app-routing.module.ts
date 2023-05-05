5090a9e6aa968f4588813fff0b5e7d4dc9d19fec
Danilo Santos <artededan@protonmail.ch>
2023-05-05 20:27:41 -0300

    (anything but me) app-routing: adicione roteamento dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/gerenciar/admin/admin.component';
+import { PessoasComponent } from './components/admin/gerenciar/pessoas/pessoas.component';
+import { VeiculosComponent } from './components/admin/gerenciar/veiculos/veiculos.component';
+import { LoginComponent } from './components/admin/login/login.component';
+import { HomeComponent } from './components/admin/home/home.component';
+import { PeopleComponent } from './components/worldwide/people/people.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/gerenciar/admin',
+    component: AdminComponent
+  },
+  {
+    path: 'admin/gerenciar/pessoas',
+    component: PessoasComponent
+  },
+  {
+    path: 'admin/gerenciar/veiculos',
+    component: VeiculosComponent
+  },
+  {
+    path: 'admin/login',
+    component: LoginComponent
+  },  
+  {
+    path: 'admin/home',
+    component: HomeComponent
+  },
+  {
+    path: 'worldwide/people',
+    component: PeopleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
