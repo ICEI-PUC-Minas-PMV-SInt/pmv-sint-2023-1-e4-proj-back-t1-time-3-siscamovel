8db7536f2371faa9d20006fb99bbd4fd59cf27ea
Danilo Santos <artededan@protonmail.ch>
2023-05-07 19:26:01 -0300

    Revert "(anything but me) Oculte a opção de gerenciamento de admin e usuários"
    This reverts commit bddedb5a0c7ee7f1bfafd14c5bc89edbc4af1e1d.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-/*
 import { AdminComponent } from './components/admin/gerenciar/admin/admin.component';
 import { PessoasComponent } from './components/admin/gerenciar/pessoas/pessoas.component';
 import { VeiculosComponent } from './components/admin/gerenciar/veiculos/veiculos.component';
 import { LoginComponent } from './components/admin/login/login.component';
 import { HomeComponent } from './components/admin/home/home.component';
 import { PeopleComponent } from './components/worldwide/people/people.component';
-*/
 import { HelloSiscmComponent } from './components/siscamovel/hello-siscm/hello-siscm.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HelloSiscmComponent
-  }/*,
+  },
   {
     path: 'admin/gerenciar/admin',
     component: AdminComponent
@@ -38,7 +36,7 @@ const routes: Routes = [
   {
     path: 'worldwide/people',
     component: PeopleComponent
-  }*/
+  }
 ];
 
 @NgModule({
