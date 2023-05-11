877df26fea966da47fcf8df4dd65b3efff5e90cf
Danilo Santos <artededan@protonmail.ch>
2023-05-11 17:42:49 -0300

    (anything but me): components: gerenciar veículos: Adicione dados de listagem de veículos

--- src/app/components/admin/gerenciar/veiculos/veiculos.component.ts
+++ src/app/components/admin/gerenciar/veiculos/veiculos.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Veiculos } from 'src/app/models/veiculos.model';
 
 @Component({
   selector: 'app-veiculos',
   templateUrl: './veiculos.component.html',
   styleUrls: ['./veiculos.component.css']
 })
-export class VeiculosComponent {
+export class VeiculosComponent implements OnInit {
 
+  veiculos: Veiculos[] = [
+    {
+      fabricante: 'BYD',
+      modelo: 'TAN EV',
+      ano: 2022,
+      capacidade: '7 lugares',
+      potencia: '272 CV',
+      torque: '35,7 kgfm',
+      velocidadeMaxima: '186 km/h',
+      tempo: '4,6 s',
+      volumePortaMalas: '235/940/1655 L',
+      pneusRodas: '265/40 R22',
+      garantia: '5 anos',
+      preco: '529.890'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.veiculos.push()
+  }
 }
