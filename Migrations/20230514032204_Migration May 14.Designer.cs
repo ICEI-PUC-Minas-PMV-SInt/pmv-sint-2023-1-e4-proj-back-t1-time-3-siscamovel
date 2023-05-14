266cef5d9edaa85d912907a5647e25b0b8353d60
Danilo Santos <artededan@protonmail.ch>
2023-05-14 01:24:03 -0300

    (SISCM-DM) Migrations: Update Database [20230514]

--- Migrations/20230514032204_Migration May 14.Designer.cs
+++ Migrations/20230514032204_Migration May 14.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using siscm_data_management.Database;
 
@@ -10,9 +11,11 @@ using siscm_data_management.Database;
 namespace siscm_data_management.Migrations
 {
     [DbContext(typeof(DatabaseSettings))]
-    partial class DatabaseSettingsModelSnapshot : ModelSnapshot
+    [Migration("20230514032204_Migration May 14")]
+    partial class MigrationMay14
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230514032204_Migration May 14.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace siscm_data_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationMay14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "admins",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    NomeAdmin = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Siscamail = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_admins", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "admins");
+        }
+    }
+}
