4ef651b00d59f0f419c06aa49ee760225fba3fda
Danilo Santos <artededan@protonmail.ch>
2023-05-19 18:04:48 -0300

    Revert "(anything but me) mais um teste"
    This reverts commit ffd681a034a93e4a75e81b9bd8a2726f67b1c4d2.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     component: HelloSiscmComponent
   },
   {
-    path: 'admin/gerenciar/admin',
+    path: '#/admin/gerenciar/admin',
     component: AdminComponent
   },
   {
