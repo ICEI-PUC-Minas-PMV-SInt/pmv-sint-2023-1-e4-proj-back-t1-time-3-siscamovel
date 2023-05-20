2499ec9a5f8ed733fcce31c7c3f8c6f6ee805b86
Danilo Santos <artededan@protonmail.ch>
2023-05-20 14:07:57 -0300

    (anything but me) components: comparaê: adicione página piloto

---
+++ src/app/components/siscamovel/comparae/comparae.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comparae',
+  templateUrl: './comparae.component.html',
+  styleUrls: ['./comparae.component.css']
+})
+export class ComparaeComponent {
+
+}
